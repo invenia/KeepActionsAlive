44eb5e8b8634358b9a541e2d8786fcc6077c3057
Wynand Badenhorst <wynand.badenhorst@invenia.ca>
2021-07-13 15:08:51 -0500

    factored out auth

--- src/lambda.py
+++ src/lambda.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     organization = os.getenv("GH_ORGANIZATION")
     user = os.getenv("GH_USER")
 
+    # Logic should be added here to support other authentication in the future
+    auth = (user, login_or_token) if user else None
     g = Github(login_or_token=login_or_token)
 
     repos = g.get_organization(organization).get_repos()
@@ -21,13 +23,14 @@ if __name__ == "__main__":
     invenia_repos = [r for r in repos if not r.fork and not r.archived]
 
     for repo in invenia_repos:
+        # Get all workflows disabled from inactivity
         disabled_workflows = [
             w for w in repo.get_workflows() if w.state == "disabled_inactivity"
         ]
 
-        if user and login_or_token:  # Not tested yet
+        if auth:  # Not tested yet
             for workflow in disabled_workflows:
                 enable_url = f"{workflow.url}/enable"
-                requests.put(enable_url, auth=(user, login_or_token))
+                requests.put(enable_url, auth=auth)
         else:
             print(disabled_workflows)
