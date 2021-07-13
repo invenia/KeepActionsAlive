8f06c188a04df41807667169ab7f3f8721763884
Wynand Badenhorst <wynand.badenhorst@invenia.ca>
2021-07-13 14:49:04 -0500

    Basic script that should work, not tested

--- src/lambda.py
+++ src/lambda.py
@@ -1,22 +1,32 @@
 from github import Github
+import os
 import pprint
 import requests
 
 pp = pprint.PrettyPrinter(indent=4)
 
-name = "mattBrzezinski"
-pat = "redacted"
+login_or_token = os.getenv(
+    "GH_LOGIN_OR_TOKEN"
+)  # Passwords not supported in this implementation
+organization = os.getenv("GH_ORGANIZATION")
+user = os.getenv("GH_USER")
 
-g = Github(pat)
 
-repos = g.get_user().get_repos()
+g = Github(login_or_token=login_or_token)
+
+repos = g.get_organization(organization).get_repos()
 
 # Get all repos that belong to Invenia and are not forks and are not archived
-invenia_repos = [r for r in repos if r.owner.login == "invenia" and not r.fork and not r.archived]
+invenia_repos = [r for r in repos if not r.fork and not r.archived]
 
-for repo in repos:
-    workflows = repo.get_workflows()
+for repo in invenia_repos:
+    disabled_workflows = [
+        w for w in repo.get_workflows() if w.state == "disabled_inactivity"
+    ]
 
-    for workflow in workflows:
-        enable_url = workflow.url + "/enable"
-        requests.put(enable_url, auth=(name, pat))
+    if user and login_or_token:  # Not tested yet
+        for workflow in disabled_workflows:
+            enable_url = f"{workflow.url}/enable"
+            requests.put(enable_url, auth=(user, login_or_token))
+    else:
+        print(disabled_workflows)
